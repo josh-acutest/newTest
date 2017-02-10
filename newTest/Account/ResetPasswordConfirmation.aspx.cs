fcc22f99a5a17dccb1b264a138ea6cf910ba419c
josh-acutest <josh.crossley@acutesales.co.uk>
2017-02-10 21:39:50 +0000

    test

---
+++ newTest/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace newTest.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ newTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="newTest.Global" Language="C#" %>
